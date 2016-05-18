9c02c6e6023af76bdabe1cafe7fc4312ae7d3d34
Deniz Tetik <deniztetik31@gmail.com>
2016-05-18 16:37:48 -0400

    _.extend and _.defaults completed

--- src/underbar.js
+++ src/underbar.js
@@ -231,11 +231,23 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(object) {
+      _.each(object, function(value, key) {
+        obj[key] = value;
+      });
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(object) {
+      _.each(object, function(value, key) {
+        if (obj[key] === undefined) obj[key] = value;
+      });
+    });
+    return obj;
   };
 
 
